1cde6da25fa9f6251a3a83ca8ab1e5856ae2a0f4
Noppadon Sodram <knopsod@gmail.com>
2018-02-27 00:43:06 +0700

    Select newly created note. Sort notes by updatedAt

--- imports/ui/NoteListHeader.test.js
+++ imports/ui/NoteListHeader.test.js
@@ -4,18 +4,43 @@ import { mount } from 'enzyme';
 import { Meteor } from 'meteor/meteor';
 
 import { NoteListHeader } from './NoteListHeader';
+import { notes } from '../fixtures/fixtures';
 
 if ( Meteor.isClient ) {
   describe('NoteListHeader', function () {
+    let meteorCall;
+    let Session;
 
-    it('should call meteorCall on click', function () {
-      const spy = expect.createSpy();
-      const wrapper = mount( <NoteListHeader meteorCall={spy} /> );
-
-      wrapper.find('button').simulate('click');
-
-      expect(spy).toHaveBeenCalledWith('notes.insert');
+    beforeEach(function () {
+      meteorCall = expect.createSpy();
+      Session = {
+        set: expect.createSpy()
+      }
     });
 
-  })
+    it('should call meteorCall on click', function () {
+      const wrapper = mount( <NoteListHeader meteorCall={meteorCall} Session={Session} /> );
+
+      wrapper.find('button').simulate('click');
+      meteorCall.calls[0].arguments[1](undefined, notes[0]._id);
+
+      expect(meteorCall.calls[0].arguments[0]).toBe('notes.insert');
+      expect(Session.set).toHaveBeenCalledWith('selectedNoteId', notes[0]._id);
+    });
+
+    // it should net set session or failed ineert
+    // copy above test case
+    // Call func wtth error and no res
+    // expect sess.set t o hav never been called
+
+    it('should not set session for failed insert', function () {
+      const wrapper = mount( <NoteListHeader meteorCall={meteorCall} Session={Session} /> );
+
+      wrapper.find('button').simulate('click');
+      meteorCall.calls[0].arguments[1]({}, undefined);
+
+      expect(meteorCall.calls[0].arguments[0]).toBe('notes.insert');
+      expect(Session.set).toNotHaveBeenCalled();
+    });
+  });
 }
